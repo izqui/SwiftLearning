3ba49e1703828037471de6eb5c93607c505cd52f
Jorge Izquierdo <jorge@izqui.me>
2014-06-15 22:27:04 -0400

    Regular polygons algorithm

---
+++ SwiftLearning/PolygonView.swift
@@ -0,0 +1,67 @@
+//
+//  ShapeView.swift
+//  SwiftLearning
+//
+//  Created by Jorge Izquierdo on 15/06/14.
+//  Copyright (c) 2014 Jorge Izquierdo. All rights reserved.
+//
+
+import UIKit
+import QuartzCore
+
+class PolygonView: UIView {
+    
+    enum PolygonOrientation {
+        
+        case Regular, Inverted
+    }
+    init(sides n: Int, radious r: Double, color c: UIColor, orientation o: PolygonOrientation) {
+        
+        var frame = CGRect(origin: CGPoint(x: 0, y: 0), size: CGSize(width: 2.0*r, height: 2.0*r))
+        super.init(frame: frame)
+        
+        if (o == .Inverted){
+    
+            println("orientation is Inverted")
+        }
+        
+        var points: CGPoint[] = CGPoint[](count: n, repeatedValue:CGPoint())
+        points[0] = CGPoint(x: r, y:0)
+        
+        var calculations: Int = 0 //Number of points we have to find each side of the diameter
+        
+        if n % 2 == 0 {
+            
+            //If number of sides is even, place the one that is one diameter away
+            points[Int(n / 2)] = CGPoint(x: r, y: 2*r)
+            calculations = (n/2)-1
+        }
+        else {
+            
+            calculations = (n-1)/2
+        }
+        
+        for i in 1...calculations{
+            
+            //Math stuff
+            var lenght = 2.0*r*sin(Double(i)*M_PI/Double(n))
+            var angle = M_PI/2 - (Double(i)*M_PI/Double(n))
+            
+            var x = lenght*sin(angle)
+            var y = lenght*cos(angle)
+            
+            points[i] = CGPoint(x: r-x, y: y)
+            points[n-i] = CGPoint(x: r+x, y: y)
+            
+        }
+        
+        var bp = UIBezierPath()
+        
+        bp.moveToPoint(points[0])
+        for i in 0..points.count{
+            bp.addLineToPoint(points[i])
+        }
+        bp.closePath()
+    }
+}
+
--- SwiftLearning/ViewController.swift
+++ SwiftLearning/ViewController.swift
@@ -9,9 +9,17 @@
 import UIKit
 
 class ViewController: UIViewController {
-                            
+    
+    @IBOutlet var drawingView: UIView
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        self.title = "Hola"
+        drawingView.backgroundColor = UIColor.greenColor()
+        
+        drawPolygons()
+        
         // Do any additional setup after loading the view, typically from a nib.
     }
 
@@ -19,7 +27,13 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    
+    func drawPolygons() {
+        
+        let p1 = PolygonView(sides: 5, radious: 100, color: UIColor.redColor(), orientation: .Regular)
+        self.view.addSubview(p1)
+        
+    }
+    
 }
 
